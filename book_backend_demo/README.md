75cb93c3031d726337c0082e79cd33a80e375649
69161 <69161@163.com>
2025-03-18 17:38:52 +0800

    swagger-ui-express swagger-jsdoc

--- book_backend_demo/README.md
+++ book_backend_demo/README.md
@@ -263,5 +263,44 @@ app.use((err,req,res,next) => {
 })
 ```
 
+```bash
+npm install swagger-ui-express swagger-jsdoc
+```
+
+```js
+//创建swagger.js
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: '图书管理系统',
+      version: '1.0.0',
+      description: '一个简单的图书管理接口文档'
+    },
+    servers: [    
+      {
+        url: 'http://localhost:3000/',
+        description: '本地开发环境'
+      }
+    ]
+  },
+  apis: ['./index.js']
+}
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
+
+```
+
+```js
+//index.js
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+```
+
 
 
--- book_backend_demo/index.js
+++ book_backend_demo/index.js
@@ -7,6 +7,10 @@ const { body, validationResult } = require('express-validator');
 // 引入 BookReposition 模块
 const BookReposition = require('./book_reposition')
 
+// 引入 swagger-ui-express 模块
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+
 // 初始化数据，模拟 books
 const books = [
   {id:1, name:'JavaScript权威指南', author:'阮一峰', price:88.8},
@@ -31,11 +35,75 @@ app.use(express.json());
 // url编码中间件
 app.use(express.urlencoded({extended:true}));
 
+// 引入swagger文档
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 // 简单的路由
 app.get('/', (req,res) => {
   res.send('图书管理系统')
 });
 
+// components swagger
+/**
+ * @swagger
+ * components:
+ *   schemas: 
+ *     Book:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: 书籍ID
+ *         name:
+ *           type: string
+ *           description: 书籍名称
+ *         author:
+ *           type: string
+ *           description: 作者
+ *         price:
+ *           type: number
+ *           description: 价格
+ *   parameters:
+ *     bookId:
+ *       in: path
+ *       name: id
+ *       required: true
+ *       schema:
+ *         type: integer
+ *         minimum: 1
+ *         description: 书籍ID
+ *   responses:
+ *     NotFound:
+ *       description: 资源不存在
+ *     InternalServerError:
+ *       description: 服务器内部错误
+ */
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: 获取所有书籍
+ *     description: 获取所有书籍
+ *     responses:
+ *       200:
+ *         description: 书籍列表
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: 请求是否成功
+ *                 count:
+ *                   type: integer
+ *                   description: 书籍数量
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Book'
+ */
 // 路由：获取所有书籍
 app.get('/books', async(req,res) => {
   const books = await bookReposition.findAll();
@@ -45,7 +113,32 @@ app.get('/books', async(req,res) => {
     data: books
   })
 });
-
+/**
+ * @swagger
+ * /books/{id}:
+ *   get:
+ *     summary: 获取单个书籍
+ *     description: 获取单个书籍
+ *     parameters:
+ *       - $ref: '#/components/parameters/bookId'
+ *     responses:
+ *       200:
+ *         description: 书籍信息
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: 请求是否成功
+ *                 data:
+ *                   $ref: '#/components/schemas/Book'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ *       500:
+ *         $ref: '#/components/responses/InternalServerError'
+ */
 // 路由：获取单个书籍
 app.get('/books/:id',  async(req,res,next) => {
   const book = await bookReposition.findById(parseInt(req.params.id));
@@ -58,7 +151,28 @@ app.get('/books/:id',  async(req,res,next) => {
     return next(new Error('书籍不存在'))
   }
 });
-
+/**
+ * @swagger
+ * /books:
+ *   post:
+ *     summary: 创建书籍
+ *     description: 创建书籍
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       201:
+ *         description: 书籍信息
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       400:
+ *         $ref: '#/components/responses/BadRequest'
+ */
 // 路由：创建书籍
 app.post('/books', bookValidationRules, async(req,res) => {
   const errors = validationResult(req);
@@ -76,6 +190,32 @@ app.post('/books', bookValidationRules, async(req,res) => {
   const book = await bookReposition.create(newBook);
   res.status(201).json(book)
 })
+/**
+ * @swagger
+ * /books/{id}:
+ *   put:
+ *     summary: 更新书籍
+ *     description: 更新书籍
+ *     parameters:
+ *       - $ref: '#/components/parameters/bookId'
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: 书籍信息
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       400:
+ *         $ref: '#/components/responses/BadRequest'
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ */
 // 路由：更新书籍
 app.put('/books/:id', bookValidationRules,async(req,res) => {
   const errors = validationResult(req);
@@ -95,6 +235,31 @@ app.put('/books/:id', bookValidationRules,async(req,res) => {
     })
   }
 })
+/**
+ * @swagger
+ * /books/{id}:
+ *   delete:
+ *     summary: 删除书籍
+ *     description: 删除书籍
+ *     parameters:
+ *       - $ref: '#/components/parameters/bookId'
+ *     responses:
+ *       200:
+ *         description: 删除成功
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   description: 请求是否成功
+ *                 message:
+ *                   type: string
+ *                   description: 删除成功提示信息
+ *       404:
+ *         $ref: '#/components/responses/NotFound'
+ */
 // 路由：删除书籍
 app.delete('/books/:id', async(req,res) => {
   const book = await bookReposition.delete(parseInt(req.params.id));
@@ -110,6 +275,9 @@ app.delete('/books/:id', async(req,res) => {
   })
 })
 
+
+
+
 app.use((req,res,next) => {
   res.status(404).json({
     success: false,
@@ -124,6 +292,7 @@ app.use((err,req,res,next) => {
     message: '服务器内部错误'
   })
 })
+
 
 
 // 端口
